f440febd887baf9d07af58be14a1fab7fe8e38a1
help yourselfes <helpyourselfes@gmail.com>
2025-12-31 12:33:34 +0300

    routes

--- internal/router/posts_routes.go
+++ internal/router/posts_routes.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/helpyourselfes/mono-chan/internal/app/post/handlers"
 	"github.com/helpyourselfes/mono-chan/internal/app/post/service"
 	"github.com/helpyourselfes/mono-chan/internal/infrastructure/repo"
@@ -18,6 +19,16 @@ func postRoutes(log *slog.Logger, storage *sql.DB) chi.Router {
 	r := chi.NewRouter()
 
 	r.Post("/", handler.CreatePost(log))
-	// TODO: GetById, Update, Delete, List
+	r.Get("/post/{boardKey}/{id}", handler.GetById(log))
+	r.Put("/", handler.Update(log))
+	r.Delete("/", handler.UserDelete(log))
+	r.Get("/list/{boardKey}/{threadId}", handler.List(log))
+
+	r.Route("/admin", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("mono-chan", map[string]string{
+			"admin": "admin", // TODO: make autorisation
+		}))
+		r.Delete("/", handler.AdminDelete(log))
+	})
 	return r
 }
